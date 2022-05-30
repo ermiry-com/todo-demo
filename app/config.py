e35cccc5d2d6cd9ec238dd0cbe1127bca88f13d0
ermiry <erick.salas@ermiry.com>
2022-05-30 18:47:12 -0500

    Getting time service address from environment

--- app/config.py
+++ app/config.py
@@ -6,7 +6,11 @@ RUNTIME = runtime_from_string (os.environ.get ("RUNTIME"))
 
 PORT = int (os.environ.get ("PORT"))
 
+TIME_SERVICE_ADDRESS = os.environ.get ("TIME_SERVICE_ADDRESS")
+
 def todo_config ():
 	print ("RUNTIME: ", runtime_to_string (RUNTIME))
 
 	print ("PORT: ", PORT)
+
+	print ("TIME_SERVICE_ADDRESS: ", TIME_SERVICE_ADDRESS)
